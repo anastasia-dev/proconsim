3843882da1eb2f66a5335a6df7e1c023bf3d2306
anastasia <marinaanastasiawork@gmail.com>
2019-11-11 14:45:30 +0300

    chore(project): commit some weird cash

--- bitrix/catalog_export/yandex.php
+++ bitrix/catalog_export/yandex.php
@@ -1,6 +1,6 @@
 <?if (!isset($_GET["referer1"]) || strlen($_GET["referer1"])<=0) $_GET["referer1"] = "yandext"?><? $strReferer1 = htmlspecialchars($_GET["referer1"]); ?><?if (!isset($_GET["referer2"]) || strlen($_GET["referer2"])<=0) $_GET["referer2"] = "";?><? $strReferer2 = htmlspecialchars($_GET["referer2"]); ?><? header("Content-Type: text/xml; charset=windows-1251");?><?echo "<?xml version=\"1.0\" encoding=\"windows-1251\"?>"?>
 <!DOCTYPE yml_catalog SYSTEM "shops.dtd">
-<yml_catalog date="2019-11-06 11:45">
+<yml_catalog date="2019-11-09 17:27">
 <shop>
 <name>опнйнмяхл</name>
 <company>опнйнмяхл</company>
